59f2e1ffacaf97e62f4d6593be5342acb835983c
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-12-27 01:05:08 +0530

    Create service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "safety-app-v1";
+const urlsToCache = [
+  "/",
+  "/landing.html",
+  "/user",
+  "/static/images/icon-192x192.png",
+  "/static/images/icon-512x512.png",
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+// Fetch Requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(() => caches.match("/user"));
+    })
+  );
+});
